779042fe8b92d517dd891fedc1074b1187c2bf02
unknown <bhuvanesheshkumar328@gmail.com>
2021-01-03 06:28:11 +0530

    image hover added

--- src/components/Work/Web.js
+++ src/components/Work/Web.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Tilt from 'react-parallax-tilt';
 import imgTree from '../../assets/img/web1.jpg';
+import ImageHover from '../common/ImageHover/ImageHover';
 
 export default class Web extends Component {
   render() {
@@ -11,89 +12,7 @@ export default class Web extends Component {
             id="national-geographic-learning"
             class="mx-auto pt-0 offseted justify-content-center d-flex workboxcol col-sm-10 col-md-5"
           >
-            <div
-              aria-label="National<br />Geographic<br />Learning"
-              data-tilt=""
-              data-tilt-gyroscope="false"
-              data-tilt-scale="1.05"
-              data-tilt-speed="200"
-              data-tilt-glare="true"
-              data-tilt-max-glare="0.2"
-              data-tilt-perspective="700"
-              data-hover=""
-              class="project-box h"
-            >
-              {/* <div class="spinner-border-wrp">
-                <span role="status" class="spinner-border">
-                  <span class="sr-only">Spinning</span>
-                </span>
-              </div> */}{' '}
-              <Tilt
-                className="parallax-effect-img project-image"
-                perspective={500}
-                glareEnable={true}
-                glareMaxOpacity={0.45}
-                scale={1.02}
-                width={500}
-                height={500}
-                scale={1.1}
-                transitionSpeed={500}
-              >
-                {/* <img src={imgTree} className="parallax-effect-img" alt="pic" /> */}
-                {/* <div className="">
-                  <div>React</div>
-                  <div>Parallax Tilt</div>
-                  <div>👀</div>
-                </div> */}
-                <div class="project-meta inner-element">
-                  <div class="block-reveal projects block-reveal--active">
-                    <span class="block-reveal__block white"></span>{' '}
-                    <span class="block-reveal__text">
-                      <br />
-                      Hotel
-                      <br />z Rowerem
-                    </span>
-                  </div>{' '}
-                  <div class="divider"></div>{' '}
-                  <div class="project-nav block-reveal block-reveal--active">
-                    <span class="block-reveal__block"></span>{' '}
-                    <span class="block-reveal__text">
-                      01
-                      <br /> <span class="arr">→</span>
-                    </span>
-                  </div>
-                </div>
-              </Tilt>
-              {/* <div class="project-meta">
-                <div class="block-reveal projects block-reveal--active">
-                  <span class="block-reveal__block white"></span>{' '}
-                  <span class="block-reveal__text">
-                    National
-                    <br />
-                    Geographic
-                    <br />
-                    Learning
-                  </span>
-                </div>{' '}
-                <div class="divider"></div>{' '}
-                <div class="project-nav block-reveal block-reveal--active">
-                  <span class="block-reveal__block"></span>{' '}
-                  <span class="block-reveal__text">
-                    00
-                    <br /> <span class="arr">→</span>
-                  </span>
-                </div>
-              </div> */}
-              <div
-                class="js-tilt-glare"
-                //   style="position: absolute; top: 0px; left: 0px; width: 100%; height: 100%; overflow: hidden; pointer-events: none;"
-              >
-                <div
-                  class="js-tilt-glare-inner"
-                  // style="position: absolute; top: 50%; left: 50%; pointer-events: none; background-image: linear-gradient(0deg, rgba(255, 255, 255, 0) 0%, rgb(255, 255, 255) 100%); width: 820px; height: 820px; transform: rotate(180deg) translate(-50%, -50%); transform-origin: 0% 0%; opacity: 0;"
-                ></div>
-              </div>
-            </div>
+            <ImageHover imgClass="webFirstBg" />
           </div>
           <div
             id="hotel-z-rowerem"
---
+++ src/components/common/ImageHover/ImageHover.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import Tilt from 'react-parallax-tilt';
+export default class ImageHover extends Component {
+  render() {
+    return (
+      <div class="project-box h">
+        {/* <div class="spinner-border-wrp">
+                <span role="status" class="spinner-border">
+                  <span class="sr-only">Spinning</span>
+                </span>
+              </div> */}{' '}
+        <Tilt
+          className={'parallax-effect-img project-image ' + this.props.imgClass}
+          perspective={500}
+          glareEnable={true}
+          glareMaxOpacity={0.45}
+          scale={1.02}
+          width={500}
+          height={500}
+          scale={1.1}
+          transitionSpeed={500}
+        >
+          <div class="project-meta inner-element">
+            <div class="block-reveal projects block-reveal--active">
+              <span class="block-reveal__block white"></span>{' '}
+              <span class="block-reveal__text">
+                <br />
+                Hotel
+                <br />z Rowerem
+              </span>
+            </div>{' '}
+            <div class="divider"></div>{' '}
+            <div class="project-nav block-reveal block-reveal--active">
+              <span class="block-reveal__block"></span>{' '}
+              <span class="block-reveal__text">
+                01
+                <br /> <span class="arr">→</span>
+              </span>
+            </div>
+          </div>
+        </Tilt>
+      </div>
+    );
+  }
+}
